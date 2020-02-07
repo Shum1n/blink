c858650bfa30093f79f8347fcd4164f2f8f1f0d0
Shum1n <158956931@qq.com>
2020-02-07 22:50:44 +0800

    组件属性，通过事件修改like属性

--- conponents/like/index.js
+++ conponents/like/index.js
@@ -4,6 +4,12 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    like:{
+      type:Boolean
+    }
+    ,count:{
+      type:Number
+    }
 
   },
 
@@ -11,13 +17,23 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    yesSrc:'images/like.png'
+    ,noSrc:'images/like@dis.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onLike:function(event){
+      // 获取like 与 count 属性
+      let like = this.properties.like
+      let count = this.properties.count
+      count = like?--count:++count
+      this.setData({
+        like:!like
+        ,count:count
+      })
+    }
   }
 })
